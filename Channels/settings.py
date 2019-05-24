a5ec75b7f1c95d31a448ebdded74f065d97c5752
harmannkibue <harmannkibue@gmail.com>
2019-05-24 07:23:57 +0300

    Allowed all hosts

--- Channels/settings.py
+++ Channels/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = '&3$aljwer31-q6jt*@(rimcflqe+ej)fk_#86151ru#!6g7eru'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -37,8 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
-
 ]
 
 MIDDLEWARE = [
